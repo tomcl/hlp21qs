1fa509b47faf65cb256b09ff294543bb6835e892
Tom Clarke <tomcl@users.noreply.github.com>
2024-01-14 09:43:12 +0000

    Update Program.fs

--- Lecture2Types/Program.fs
+++ Lecture2Types/Program.fs
@@ -1,4 +1,47 @@
-﻿//--------------------------------- id --------------------------------------//
+﻿//----------------------------- Understanding Types--------------------------//
+
+type MyType1 = int * string
+
+let a: MyType1 = (1,"a")
+let b: int * string = a
+let a1 = a
+let b1 = b
+
+type My3Tuple<'a> = 'a * int * string
+
+let a3 : My3Tuple<float> = 1.0, 1, "a3"
+let a4: My3Tuple<int list> = [], 1, "a3"
+
+let l1: int list = [1]
+let l2: list<int> = [2]
+let l3: List<int> = [3]
+let l4 = l1 @ l2 @ l3
+
+//---------------------------------------------------------------------------//
+//--------------------------Understanding Polymorphic Types------------------//
+//---------------------------------------------------------------------------//
+
+let f1 a b = a + b
+let f2 = fun a -> (fun b -> a + b)
+
+let S' f g x = (f x) (g x)
+
+let S f g x = f x (g x) // brackets round f x are not necessary
+
+// How to work out type of S?
+(*
+1. Start with unknown types of x, f, g, S
+2. f, g, S must be functions : 'a -> 'b where we need to work out 'a and 'b sepaartely for each of them
+3a. type of x
+3b. type of g
+3c. type of f
+3d. type of S
+
+
+
+
+*)
+//--------------------------------- id --------------------------------------//
 
 //    let id1 x = x
 
@@ -27,3 +70,4 @@
 //    let a2 = testArith 2 1
 
 //    let a1 = testArith 2.0  1.0
+
